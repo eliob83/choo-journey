6e824a72cd69b4e5b3c0e5e701d392c5539a1f80
Guillaume <guillaume.feltrin@isen.yncrea.fr>
2019-11-18 17:09:55 +0100

    Add icon with train png

--- src/app/components/station-map/station-map.component.ts
+++ src/app/components/station-map/station-map.component.ts
@@ -17,6 +17,18 @@ export class StationMapComponent implements OnInit {
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: 'Frugal Map'
     }).addTo(myfrugalmap);
+
+    var trainIcon = L.icon({
+      iconUrl: 'train.png',
+      shadowUrl: 'train-shadow.png',
+      iconSize:     [38, 95], // size of the icon
+      shadowSize:   [50, 64], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+    L.marker([47.0833, 2.4], {icon: trainIcon}).bindPopup('Je suis un train tchoutchou').addTo(myfrugalmap).openPopup();
+
   }
 
 }
