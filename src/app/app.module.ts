982129f02fbb54f3a1d3c1b446ccc3c2fd28e041
Nypsyy <mayeul.marsaut@hotmail.fr>
2019-11-18 16:31:33 +0100

    Setting up tree, start journey feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {StationComponent} from './station/station.component';
-import {StationListComponent} from './station-list/station-list.component';
-import {StationMapComponent} from './station-map/station-map.component';
-import {JourneyComponent} from './journey/journey.component';
+import {StationComponent} from './components/station/station.component';
+import {StationListComponent} from './components/station-list/station-list.component';
+import {StationMapComponent} from './components/station-map/station-map.component';
+import {JourneyComponent} from './components/journey/journey.component';
 
 import {AlertModule} from 'ngx-bootstrap';
 
