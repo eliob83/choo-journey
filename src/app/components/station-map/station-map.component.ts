bb5225371cee98a30e99cf224a60fe4e49a02df4
Guillaume <guillaume.feltrin@isen.yncrea.fr>
2019-11-19 11:07:30 +0100

    Try multiple markers on map

--- src/app/components/station-map/station-map.component.ts
+++ src/app/components/station-map/station-map.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import * as L from 'leaflet';
+import { StationService} from '../../services/station.service';
 
 @Component({
   selector: 'app-station-map',
@@ -8,27 +9,25 @@ import * as L from 'leaflet';
 })
 export class StationMapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private stationsService: StationService) {
+  }
 
   ngOnInit() {
     // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
-    const myfrugalmap = L.map('frugalmap').setView([47.0833, 2.4], 6);
+    const myMap = L.map('myMap').setView([47.0833, 2.4], 6);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: 'Frugal Map'
-    }).addTo(myfrugalmap);
+      attribution: 'myMap'
+    }).addTo(myMap);
 
-    const trainIcon = L.icon({
-      iconUrl: 'train.png',
-      shadowUrl: 'train-shadow.png',
-      iconSize:     [38, 95], // size of the icon
-      shadowSize:   [50, 64], // size of the shadow
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    const myIcon = L.icon({
+      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
     });
-    L.marker([47.0833, 2.4], {icon: trainIcon}).bindPopup('Je suis un train tchoutchou').addTo(myfrugalmap).openPopup();
+
+    this.stationsService.getLoadedStations().forEach(station => {
+      L.marker([+station.lat, +station.lon], {icon: myIcon}).addTo(myMap);
+      console.log(station.lat, station.lon, station.name);
+    });
 
   }
-
 }
