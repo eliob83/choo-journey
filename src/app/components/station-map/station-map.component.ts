c2f0c907ada44e7d95dd1a23491b144caafe8752
Elio <eliob83@gmail.com>
2019-11-19 22:44:21 +0100

    Icons folder and path corrected

--- src/app/components/station-map/station-map.component.ts
+++ src/app/components/station-map/station-map.component.ts
@@ -1,8 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import * as L from 'leaflet';
+import { Component, OnInit } from '@angular/core';
+
 import {StationService} from '../../services/station.service';
-import {DivIcon, Icon, IconOptions} from 'leaflet';
-import { StationType } from 'src/app/classes/Station';
+
+import * as L from 'leaflet';
+import { Icon } from 'leaflet';
+
+import { Station, StationType } from 'src/app/classes/Station';
+
 
 @Component({
   selector: 'app-station-map',
@@ -10,61 +14,77 @@ import { StationType } from 'src/app/classes/Station';
   styleUrls: ['./station-map.component.css']
 })
 export class StationMapComponent implements OnInit {
+  stationsMap: L.Map = null;
   markers: Array<any> = [];
   icons: Icon[][] = [[ ], [ ], [ ]];
+
 
   constructor(private stationsService: StationService) { }
 
   ngOnInit() {
-    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
-    const myMap = L.map('myMap').setView([47.0833, 2.4], 6);
+    // Map declaration with France-centered coords and zoom
+    this.stationsMap = L.map('stationsMap').setView([47.0833, 2.4], 6);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: 'stationsMap' }).addTo(this.stationsMap);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: 'myMap'
-    }).addTo(myMap);
 
-    this.icons[0][0] = L.icon({
-      iconUrl: '../assets/img/interrogation.png', iconSize: [50, 50], iconAnchor: [25, 25]
-    });
+    // Icons load
+    this.icons[0][0] = L.icon({ iconUrl: '../assets/map/interrogation.png', iconSize: [50, 50], iconAnchor: [25, 25] });
     for (let t = StationType.TRAIN_STATION; t <= StationType.BUS_STATION; t++) {
       for (let i = 0; i < 9; i++) {
-        this.icons[t][i] = L.icon({
-          iconUrl: '../assets/img/' + (t === StationType.TRAIN_STATION ? 'train' : 'bus') + i + '.png',
-          iconSize: [50, 50], iconAnchor: [25, 25]
-        });
+        this.icons[t][i] = L.icon(
+          {
+            iconUrl: '../assets/map/' + (t === StationType.TRAIN_STATION ? 'train-icons/train' : 'bus-icons/bus') + i + '.png',
+            iconSize: [50, 50], iconAnchor: [25, 25]
+          }
+        );
       }
     }
 
-    // When stations are searched
-    this.stationsService.stationsSubjects.subscribe((data) => {
-      this.markers.forEach(marker => {
-        myMap.removeLayer(marker);
-      });
-      this.markers.splice(0, this.markers.length);
-
-      data.forEach(station => {
-        if (+station.lat !== 0 || +station.lon !== 0) {
-          this.markers.push(
-            L.marker(
-                [+station.lat, +station.lon],
-                {icon: this.icons[station.type][Math.floor(Math.random() * this.icons[station.type].length)]})
-              .bindPopup(station.label).addTo(myMap)
-              .on('click', e => {
-                this.stationsService.mapSubjects.next(station);
-              }
-            )
-          );
-        }
-      });
+    // List update observable
+    this.stationsService.stationsSubjects.subscribe(data => {
+      this.updateMarkers(data);
     });
 
     // When a station is focused in the list
-    this.stationsService.mapSubjects.subscribe((station) => {
-      if (station !== null && station.lon !== undefined && station.lat !== undefined) {
-        if (+station.lat !== 0 && +station.lon !== 0) {
-          myMap.flyTo([+station.lat, +station.lon], 16);
-        }
+    this.stationsService.mapSubjects.subscribe(station => {
+      this.focusStation(station);
+    });
+  }
+
+
+  updateMarkers(data: Station[]) {
+    // Remove every marker from the map
+    this.markers.forEach(marker => {
+      this.stationsMap.removeLayer(marker);
+    });
+    // Clear markers array
+    this.markers.splice(0, this.markers.length);
+
+    // For each station of the list
+    data.forEach(station => {
+      // Non-zero coords (unknown)
+      if (station.lat !== 0 || station.lon !== 0) {
+        this.markers.push(L.marker(
+            [station.lat, station.lon],
+            // Random station type icon
+            { icon: this.icons[station.type][Math.floor(Math.random() * this.icons[station.type].length)] }
+            ).bindPopup(station.label).addTo(this.stationsMap).on('click', e => {
+              // Marker click push it on map observable
+              this.stationsService.mapSubjects.next(station);
+            })
+        );
       }
     });
+  }
+
+  focusStation(station: Station) {
+    // If non-zero coords on existing object
+    if (station !== null && station.lat !== 0 && station.lon !== 0) {
+      this.focusOnMap(station.lat, station.lon, 16);
+    }
+  }
+
+  focusOnMap(lat: number, lon: number, zoom: number) {
+    this.stationsMap.flyTo([lat, lon], zoom);
   }
 }
