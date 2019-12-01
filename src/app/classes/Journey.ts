2443bbb28423931815ba5e4b030b49db9f74c096
Elio <eliob83@gmail.com>
2019-12-01 23:21:12 +0100

    Moved Options, new JourneyDate, corrected Journey
    - Options renamed SearchOption and moved to Search.ts file
    - JourneyDate class created : date with right parsing for journey
    - Journey corrected : sections length rightly got for parsing

--- src/app/classes/Journey.ts
+++ src/app/classes/Journey.ts
@@ -1,10 +1,35 @@
 import {Station} from './Station';
 
-// Options for the searchStation function / request
-export enum Options {
-  FROM,
-  TO,
-  SEARCH
+export class JourneyDate {
+  day: number;
+  month: number;
+  year: number;
+
+  hour: number;
+  minute: number;
+  second: number;
+
+  constructor(args: {}) {
+    this.day = args[`day`];
+    this.month = args[`month`];
+    this.year = args[`year`];
+
+    this.hour = args[`hour`];
+    this.minute = args[`minute`];
+    this.second = args[`second`];
+  }
+
+  getYMD() {
+    return this.year + '-' + this.month + '-' + this.day;
+  }
+
+  getDMY() {
+    return this.day + '-' + this.month + '-' + this.year;
+  }
+
+  getTime() {
+    return this.hour + ':' + this.minute + ':' + this.second;
+  }
 }
 
 export class Journey {
@@ -17,7 +42,8 @@ export class Journey {
 
 
   constructor(args: Array<any>) {
+    console.log(args);
     this.from = new Station(args[`sections`][0][`from`]);
-    this.to = new Station(args[`sections`][args.length - 1][`to`]);
+    this.to = new Station(args[`sections`][args[`sections`].length - 1][`to`]);
   }
 }
